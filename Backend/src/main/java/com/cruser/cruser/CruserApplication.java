94d222cc4c700c6d9f488dc116d8239fb59951d3
Intisar <Code.g.light@gmail.com>
2025-01-26 04:17:06 +0300

    Beginning

---
+++ Backend/src/main/java/com/cruser/cruser/CruserApplication.java
@@ -0,0 +1,13 @@
+package com.cruser.cruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruserApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/cruser/cruser/CruserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cruser.cruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
